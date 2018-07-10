21440d4a032d7dddd3a21d871630c576a47170b8
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-07-10 01:08:35 -0300

    base url adjusts

--- public/push-sw.js
+++ public/push-sw.js
@@ -1,4 +1,4 @@
-const url = `/quotes.json`
+const url = `quotes.json`
 
 const loadQuote = () => fetch(url, {cache: 'force-cache'})
   .then(response => response.json())
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,18 @@
 // Inside vue.config.js
+const baseUrl = process.env.NODE_ENV === 'production'
+  ? '/anime-sapientiae/'
+  : '/'
+const pushSw = `${baseUrl}push-sw.js`
+
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/anime-sapientiae/'
-    : '/',
+  baseUrl,
   pwa: {
     name: 'Anime Sapientiae',
     themeColor: '#1a2420',
     msTileColor: '#1a2420',
     appleMobileWebAppStatusBarStyle: '#1a2420',
     workboxOptions: {
-      importScripts: ['/push-sw.js'],
+      importScripts: [pushSw],
       skipWaiting: true
     }
   }
